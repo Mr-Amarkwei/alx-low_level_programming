da1f9f41de865ce4f4bbe576a0b6f45e5c0a0c45
Mr-Amarkwei <joelamarkwei@gmail.com>
2022-04-09 10:57:36 -0700

    last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * main - where the program actually starts from
+**/
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+/**
+ *  main - is the main function where the program actually starts
+**/
 int main(void)
 {
 		printf("Size of a char: %d byte(s)\n", sizeof(char));
