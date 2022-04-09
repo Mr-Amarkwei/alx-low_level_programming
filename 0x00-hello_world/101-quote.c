0b282a753144164926aacdcfced93f1d8d7f267a
Mr-Amarkwei <joelamarkwei@gmail.com>
2022-04-09 12:08:36 -0700

    p//...

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/*
+/**
 * main - the main block
-* Description: begins the execution
+* Description: begins the execution of the whole program, this is the first function to be executed
 * Return: Always 0
 */
+
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
